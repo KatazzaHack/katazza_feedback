c9391592c4abe83a85632c0a98097a2a60f799c3
Olga Kalitova <olga.kalitova@gmail.com>
2020-10-13 23:08:23 +0300

    Add bottom navigation handling
    Now when clicking on tab page that corresponds to the tab pressed is going to be shown.

---
+++ lib/add_feedback_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddFeedbackPage extends StatefulWidget {
+  AddFeedbackPage({Key key}) : super(key: key);
+
+  @override
+  _AddFeedbackPageState createState() => _AddFeedbackPageState();
+}
+
+class _AddFeedbackPageState extends State<AddFeedbackPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("AddFeedback page"),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:katazza_feedback/add_feedback_page.dart';
+import 'package:katazza_feedback/person_page.dart';
+import 'package:katazza_feedback/search_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Katazza Feedback',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Katazza Feedback'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -52,50 +34,61 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  Widget _currentPage = SearchPage();
+  String _title = "Katazza Feedback";
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      switch (index) {
+        case 0:
+          _currentPage = SearchPage();
+          _title = "Katazza Feedback";
+          break;
+        case 1:
+          _currentPage = AddFeedbackPage();
+          _title = "Add feedback";
+          break;
+        case 2:
+          _currentPage = PersonPage();
+          _title = "Person";
+          break;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('users').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-        return Scaffold(
-          appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text(widget.title),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: Center(
+        child: _currentPage,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
           ),
-          body: Center(
-            child: Text(_selectedIndex.toString()),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            title: Text("Add"),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Center(),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add),
-                title: Center(),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: Center(),
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            backgroundColor: Colors.lightBlue,
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.black,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Person"),
           ),
-        );
-      },);
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+      ),
+    );
   }
 }
